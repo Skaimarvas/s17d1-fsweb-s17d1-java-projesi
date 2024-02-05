c52fa00fe0f6095bc1175e8092f39f972639e4ae
skaimarvas <skaimarvas@gmail.com>
2024-02-06 00:55:06 +0300

    values variables and getAppInfo were created

--- SpringFristProject/src/main/java/com/example/SpringFristProject/controller/AnimalController.java
+++ SpringFristProject/src/main/java/com/example/SpringFristProject/controller/AnimalController.java
@@ -1,9 +1,22 @@
 package com.example.SpringFristProject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/workintech")
 public class AnimalController {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("{app.version}")
+    private String appVersion;
+
+    @GetMapping("/app-info")
+    public String getAppInfo(){
+        return "App Name: " + appName + ", App Version: " + appVersion;
+    }
 }
