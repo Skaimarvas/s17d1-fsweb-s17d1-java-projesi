6a109d78a41d68aab60739d49137265b73ac45a2
skaimarvas <skaimarvas@gmail.com>
2024-02-06 00:28:25 +0300

    Dependecies were installed.

---
+++ SpringFristProject/src/main/java/com/example/SpringFristProject/SpringFristProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringFristProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFristProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFristProjectApplication.class, args);
+	}
+
+}
---
+++ SpringFristProject/src/test/java/com/example/SpringFristProject/SpringFristProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringFristProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFristProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
