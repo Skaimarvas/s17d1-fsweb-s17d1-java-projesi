e11232b4ddaea0ac406dcbb2b437b6883400f50e
skaimarvas <skaimarvas@gmail.com>
2024-02-06 11:55:37 +0300

    empty constructar was created  for creating bean

--- SpringFristProject/src/main/java/com/example/SpringFristProject/entity/Animal.java
+++ SpringFristProject/src/main/java/com/example/SpringFristProject/entity/Animal.java
@@ -1,9 +1,15 @@
 package com.example.SpringFristProject.entity;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Animal {
     private int id;
     private String name;
 
+    public Animal(){
+
+    }
     public Animal(int id, String name) {
         this.id = id;
         this.name = name;
